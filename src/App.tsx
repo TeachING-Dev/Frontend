cfc07dab29fcc57bdcb77e63c2f7d08a7eddc54f
Kimsubinnn <bella8010@naver.com>
2026-07-08 22:55:16 +0900

    feat: 로그인 페이지 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import ArchivePage from "./pages/ArchivePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route element={<MainLayout />}>
           <Route path="/" element={<ArchivePage />} />
         </Route>
