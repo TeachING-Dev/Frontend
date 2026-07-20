e41a66194ea0fbe4d981d6ef60ae2f34c91cec06
Eloise0149 <yoonseo08130404@gmail.com>
2026-07-20 21:29:31 +0900

    merge: resolve App route conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 ﻿import {
   BrowserRouter,
-  Navigate,
   Route,
   Routes,
 } from "react-router-dom";
 
+import HomeLayout from "./layouts/HomeLayout";
 import MainLayout from "./layouts/MainLayout";
 import ArchiveDataPage from "./pages/ArchiveDataPage";
 import ArchiveFolderPage from "./pages/ArchiveFolderPage";
 import ArchivePage from "./pages/ArchivePage";
 import ChatbotPage from "./pages/ChatbotPage";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupCompletePage from "./pages/SignupCompletePage";
 import SignupPage from "./pages/SignupPage";
+import SubscriptionCompletePage from "./pages/SubscriptionCompletePage";
+import SubscriptionPage from "./pages/SubscriptionPage";
 import TeachingMapCreatePage from "./pages/TeachingMapCreatePage";
 import TeachingMapPage from "./pages/TeachingMapPage";
 
@@ -20,32 +23,45 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        {/* 인증 페이지 */}
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+
         <Route
           path="/signup/complete"
           element={<SignupCompletePage />}
         />
 
-        <Route element={<MainLayout />}>
-          {/* 기본 주소 → /archive로 이동 */}
+        <Route
+          path="/subscription/complete"
+          element={<SubscriptionCompletePage />}
+        />
+
+        {/* 홈 전용 레이아웃 */}
+        <Route element={<HomeLayout />}>
           <Route
             path="/"
-            element={<Navigate to="/archive" replace />}
+            element={<HomePage />}
           />
+        </Route>
 
+        {/* 기본 레이아웃 페이지 */}
+        <Route element={<MainLayout />}>
           {/* 보관함 */}
           <Route
             path="/archive"
             element={<ArchivePage />}
           />
 
-          {/* 폴더 상세 */}
+          {/* 보관함 폴더 상세 */}
           <Route
             path="/archive/folder"
             element={<ArchiveFolderPage />}
           />
 
-          {/* 자료 상세 */}
+          {/* 보관함 자료 상세 */}
           <Route
             path="/archive/folder/data/:dataId"
             element={<ArchiveDataPage />}
@@ -62,8 +78,15 @@ function App() {
             path="/teaching-map/create"
             element={<TeachingMapCreatePage />}
           />
+
+          {/* 구독 */}
+          <Route
+            path="/subscription"
+            element={<SubscriptionPage />}
+          />
         </Route>
 
+        {/* 챗봇 페이지 */}
         <Route element={<MainLayout insetMenu />}>
           <Route
             path="/chatbot"
@@ -71,6 +94,7 @@ function App() {
           />
         </Route>
 
+        {/* 회원가입 페이지 */}
         <Route element={<MainLayout showRightIcons={false} />}>
           <Route
             path="/signup"
