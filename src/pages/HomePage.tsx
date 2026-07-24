b69415efdf1154f976c3568ef2bed3fa9be9d5b2
nonoididnt <gnh08033@gmail.com>
2026-07-24 16:41:30 +0900

    feat: 홈화면 반응형 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,24 @@ import HomeHeader from "../components/home/HomeHeader";
 
 const HomePage = () => {
   return (
-    <main className="mx-auto flex w-[1120px] flex-col gap-[15px] pt-[60px] py-[75px]">
+    <main
+      className="
+        mx-auto
+        flex
+        w-full
+        max-w-[1120px]
+        flex-col
+        gap-[15px]
+        px-[20px]
+        pb-[100px]
+        pt-[30px]
+        md:px-[40px]
+        md:pt-[45px]
+        lg:px-0
+        lg:pb-[75px]
+        lg:pt-[60px]
+      "
+    >
       <HomeHeader />
       <HomeContent />
     </main>
