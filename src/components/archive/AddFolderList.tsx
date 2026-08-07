4c812d109536fa404625661f0243d8cee1a12909
Kimsubinnn <bella8010@naver.com>
2026-08-08 01:07:44 +0900

    feat:챗봇 사이드바 수정

---
+++ src/components/archive/AddFolderList.tsx
@@ -0,0 +1,24 @@
+type AddFolderItemProps = {
+  onClick?: () => void;
+};
+
+const AddFolderList = ({ onClick }: AddFolderItemProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className="flex h-[88px] w-full items-center border-b border-[#2A2737] hover:bg-white/5"
+    >
+      <img
+        src="/folder-add.png"
+        alt="새 폴더"
+        className="h-[64px] w-[64px]"
+      />
+
+      <span className="ml-[22px] text-[24px] font-semibold text-[#B79CFF]">
+        새 폴더 추가
+      </span>
+    </button>
+  );
+};
+
+export default AddFolderList;
