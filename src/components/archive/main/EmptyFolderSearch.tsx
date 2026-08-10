8580af431a4898c90792b367cb3d1a03219992ca
nonoididnt <gnh08033@gmail.com>
2026-08-11 01:22:26 +0900

    feat: URL 상세분석 처리 로직 수정

--- src/components/archive/main/EmptyFolderSearch.tsx
+++ src/components/archive/main/EmptyFolderSearch.tsx
@@ -8,7 +8,7 @@ const EmptyArchiveSearch = () => {
       message="검색결과가 존재하지 않습니다."
       containerClassName="flex flex-col items-center pt-[180px]"
       imageClassName="mb-[10px] h-[91.2px] w-[95.9px] object-contain lg:mb-8 lg:h-[200px] lg:w-[200px]"
-      messageClassName="w-full whitespace-nowrap text-center font-['SUIT'] text-[14px] font-normal leading-[150%] tracking-[-0.35px] text-[#717379] lg:font-['42dot_Sans'] lg:text-[28px] lg:font-semibold lg:tracking-[-0.84px] lg:text-[#F5F2FF]"
+      messageClassName="w-full whitespace-nowrap text-center text-[14px] font-normal not-italic leading-[150%] tracking-[-0.35px] text-[#717379] lg:text-[24px] lg:font-semibold lg:tracking-[-0.72px]"
     />
   );
 };
