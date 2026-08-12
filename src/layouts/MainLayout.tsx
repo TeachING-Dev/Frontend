e1777f4018c605c144c84238f256189886f3e289
Eloise0149 <yoonseo08130404@gmail.com>
2026-08-12 16:23:55 +0900

    feat: QA 3차 반영

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,7 +9,6 @@ type MainLayoutProps = {
   insetMenu?: boolean;
   showMenuIcon?: boolean;
   hideHeaderOnMobile?: boolean;
-  hideHeader?: boolean;
   hideMobileNav?: boolean;
 };
 
@@ -18,7 +17,6 @@ const MainLayout = ({
   insetMenu = false,
   showMenuIcon = true,
   hideHeaderOnMobile = false,
-  hideHeader = false,
   hideMobileNav = false,
 }: MainLayoutProps) => {
   const location = useLocation();
@@ -39,7 +37,7 @@ const MainLayout = ({
   return (
     <div className="min-h-screen bg-[#0B0A18]">
       {/* 모바일은 56px 투명 헤더, PC는 64px 고정 헤더 */}
-      {!hideHeader && <div
+      <div
         className={`relative z-50 h-[56px] bg-transparent lg:fixed lg:inset-x-0 lg:top-0 lg:block lg:h-16 ${
           usesPageOwnedMobileHeader ? "hidden" : ""
         }`}
@@ -50,7 +48,7 @@ const MainLayout = ({
           showMenuIcon={showMenuIcon}
           onMenuClick={handleOpenSidebar}
         />
-      </div>}
+      </div>
 
       <Sidebar
         open={isSidebarOpen}
@@ -64,7 +62,7 @@ const MainLayout = ({
         className="pointer-events-none fixed inset-x-0 bottom-0 top-0 z-0 bg-[linear-gradient(to_bottom,#0B0A18_0%,#0B0A18_72%,#18152D_88%,#30265F_100%)] lg:top-[64px]"
       />
 
-      <main className={`relative z-10 min-h-screen ${hideHeader ? "" : "lg:pt-16"}`}>
+      <main className="relative z-10 min-h-screen lg:pt-16">
         <Outlet />
       </main>
     </div>
